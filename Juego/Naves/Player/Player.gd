e5b73ae8c15893a8f9159d0adbf8f6154b5d3018
ismael29h <ismael29h@gmail.com>
2022-10-07 17:52:33 -0300

    Script agregado a nave: 'Player' y se ha creado el proyectil del mismo

---
+++ Juego/Naves/Player/Player.gd
@@ -0,0 +1,41 @@
+class_name Player
+
+extends RigidBody2D
+
+export var potencia_motor:int = 20
+export var potencia_rotacion:int = 280
+
+var empuje:Vector2 = Vector2.ZERO
+var dir_rotacion:int = 0
+
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+		# aplicar impulso: adelante-atrás
+	apply_central_impulse(empuje.rotated(rotation))
+	# aplicar torque de rotación
+	apply_torque_impulse(dir_rotacion * potencia_rotacion)
+	
+	# debbug
+	#print(rad2deg(rotation))
+	#print(empuje.rotated(rotation))
+
+func _process(delta: float) -> void:
+	player_input()
+
+
+func player_input() -> void:
+	# empuje
+	empuje = Vector2.ZERO
+	if Input.is_action_pressed("mover_adelante"):
+		empuje = Vector2(potencia_motor, 0)
+	elif Input.is_action_pressed("mover_atrás"):
+		empuje = Vector2(-potencia_motor, 0)
+
+	# rotación
+	dir_rotacion = 0
+	if Input.is_action_pressed("mover_antihorario"):
+		dir_rotacion -= 1
+	elif Input.is_action_pressed("mover_horario"):
+		dir_rotacion += 1
+
+
---
+++ Juego/Proyectiles/Proyectil.gd
@@ -0,0 +1,16 @@
+class_name Proyectil
+extends Area2D
+
+var velocidad:Vector2 = Vector2.ZERO
+var danio:float
+
+
+func _physics_process(delta: float) -> void:
+	position += velocidad * delta
+
+
+# constructor
+func crear(pos:Vector2, dir:float, vel: float, danio_p:int) -> void:
+	position = pos
+	rotation = dir
+	velocidad = Vector2(vel, 0).rotated(dir)
