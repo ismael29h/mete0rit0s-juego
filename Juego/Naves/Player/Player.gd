f3500dfc0cb353be46b66a568678b90635911830
ismael29h <ismael29h@gmail.com>
2022-10-10 19:13:22 -0300

    Agregado estela de la nave Player

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -4,12 +4,15 @@ class_name Player
 
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima:int = 100
+
 
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $PuntoEstela/Trail2D
 
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
@@ -22,17 +25,21 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	#print(rad2deg(rotation))
 	#print(empuje.rotated(rotation))
 
-func _process(delta: float) -> void:
-	player_input()
+#func _process(delta: float) -> void:
+	#player_input()
+	
 
 
-func player_input() -> void:
+#func player_input() -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	# empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
 		empuje = Vector2(potencia_motor, 0)
+		estela.set_max_points(estela_maxima)
 	elif Input.is_action_pressed("mover_atrás"):
 		empuje = Vector2(-potencia_motor, 0)
+		estela.set_max_points(0)
 
 	# rotación
 	dir_rotacion = 0
@@ -42,8 +49,10 @@ func player_input() -> void:
 		dir_rotacion += 1
 
 	# disparar
-	if Input.is_action_pressed("disparo_principal"):
-		canion.set_esta_disparando(true)
+	if Input.is_action_pressed("disparo_principal"):	
+		# el disparo principal no se realizará si el disparo secundario está activo
+		if not laser.is_casting:
+			canion.set_esta_disparando(true)
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
 
@@ -52,5 +61,7 @@ func player_input() -> void:
 		if laser.is_casting:
 			laser.set_is_casting(false)
 		else:
+			# el disparo secundario anula el disparo principal
+			canion.set_esta_disparando(false)
 			laser.set_is_casting(true)
 		
