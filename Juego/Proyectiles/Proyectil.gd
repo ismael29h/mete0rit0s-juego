7fa307053d7636a3b1c69b86e79fd1229ab587a1
ismael29h <ismael29h@gmail.com>
2022-10-12 11:29:39 -0300

    Daño implementado

--- Juego/Proyectiles/Proyectil.gd
+++ Juego/Proyectiles/Proyectil.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 class_name Proyectil
 
+
+export var danio:float = 2.0
+
 var velocidad:Vector2 = Vector2.ZERO
-var danio:float
 
 
 func _physics_process(delta: float) -> void:
@@ -17,5 +19,21 @@ func crear(pos:Vector2, dir:float, vel: float, danio_p:int) -> void:
 	velocidad = Vector2(vel, 0).rotated(dir)
 
 
-func _on_VisibilityNotifier2D_screen_exited():
+# cuando provoca daño
+func daniar(cuerpo:CollisionObject2D) -> void:
+	if cuerpo.has_method("recibir_danio"):
+		cuerpo.recibir_danio(danio)
 	queue_free()
+
+
+# sale de la línea de visión
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
+
+# golpea a un enemigo
+func _on_area_entered(area:Area2D) -> void:
+	daniar(area)
+	
+
+func _on_body_entered(body:Node) -> void:
+	daniar(body)
--- Juego/Test/EnemigoDummy.gd
+++ Juego/Test/EnemigoDummy.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
-
-func _ready():
-	pass
+export var vida:float = 10.0
 
 
 func _on_Area2D_body_entered(body):
 	if body is Player:
 		body.destruir()
+
+
+func recibir_danio(danio:float) -> void:
+	vida -= danio
+	if vida <= 0.0:
+		queue_free()
