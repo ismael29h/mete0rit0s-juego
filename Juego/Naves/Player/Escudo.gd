ec549082951646e52837b4a88992000e1ab09b06
ismael29h <ismael29h@gmail.com>
2022-10-13 18:29:40 -0300

    Energía agregada al escudo

--- Juego/Naves/Player/Escudo.gd
+++ Juego/Naves/Player/Escudo.gd
@@ -2,20 +2,41 @@ extends Area2D
 
 class_name Escudo
 
+export var energia:float = 10.0
+export var desgaste:float = 2.0 # 10/5=2seg
+
+
 var esta_activado:bool = false setget, get_esta_activado
 
 func _ready() -> void:
+	set_process(false)
 	controlar_colisionador(true)
 
+
+func _process(delta) -> void:
+	energia -= desgaste * delta
+	print(energia)
+	if energia <= 0.0:
+		desactivar()
 
 func controlar_colisionador(esta_desactivado:bool) -> void:
 	$CollisionShape2D.set_deferred("disabled", esta_desactivado)
 
 
 func activar() -> void:
+	if energia <= 0.0:
+		return
+
 	esta_activado = true
 	controlar_colisionador(false)
 	$AnimationPlayer.play("activando")
+
+
+func desactivar() -> void:
+	set_process(false)
+	esta_activado = false
+	controlar_colisionador(true)
+	$AnimationPlayer.play_backwards("activando")
 
 
 func get_esta_activado() -> bool:
@@ -23,5 +44,6 @@ func get_esta_activado() -> bool:
 
 
 func _on_AnimationPlayer_animation_finished(anim_name:String) -> void:
-	if anim_name == "activando":
+	if anim_name == "activando" and esta_activado:
 		$AnimationPlayer.play("activado")
+		set_process(true)
