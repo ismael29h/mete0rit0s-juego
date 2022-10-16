c742104fac0c8708f30843381dbce4a66cc05582
ismael29h <ismael29h@gmail.com>
2022-10-16 18:43:03 -0300

    Aleatoriedad al sector meteorito

--- Juego/Niveles/SectorMeteoritos.gd
+++ Juego/Niveles/SectorMeteoritos.gd
@@ -1,9 +1,29 @@
 extends Node2D
 class_name SectorMeteoritos
 
+export var cantidad_meteoritos:int = 10
 
-# one_shot / autostart
-func _on_Timer_timeout() -> void:
+var spawners:Array
+
+func _ready() -> void:
+	almacen_spawners()
+
+
+func almacen_spawners() -> void:
 	for spawner in $Spawners.get_children():
-		spawner.spawnear_meteorito()
-		print('ok')
+		spawners.append(spawner)
+
+
+func spawner_random() -> int:
+	randomize()
+	return randi() % spawners.size()
+
+
+# autostart
+func _on_Timer_timeout() -> void:
+	if cantidad_meteoritos == 0:
+		$Timer.stop()
+		return
+	
+	(spawners[spawner_random()]).spawnear_meteorito()
+	cantidad_meteoritos -= 1
