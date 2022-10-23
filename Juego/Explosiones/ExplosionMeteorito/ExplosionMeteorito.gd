7b01362997c1ed36ba1b4d4617ed2a6c0b0d2df3
ismael29h <ismael29h@gmail.com>
2022-10-23 11:09:43 -0300

    Variedad a las animaciones de destrucción de meteorito

--- Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
+++ Juego/Explosiones/ExplosionMeteorito/ExplosionMeteorito.gd
@@ -1,6 +1,21 @@
 extends Node2D
 class_name ExplosionMeteorito
 
+onready var lista_animacion:Array = $DestruccionAnimation.get_animation_list()
+
+func _ready() -> void:
+	$DestruccionAnimation.play(explosion_random())
+	print(lista_animacion)
+
+func explosion_random() -> String:
+	randomize()
+	var num_anim:int = $DestruccionAnimation.get_animation_list().size()
+	print(num_anim)
+	var indice_anim:int = randi() % num_anim
+	print(indice_anim)
+	print(lista_animacion[indice_anim])
+	return lista_animacion[indice_anim]
+
 
 func _on_DestruccionAnimation_animation_finished(anim_name:String) -> void:
 	queue_free()
