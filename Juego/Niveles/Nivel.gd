60fe52d5b1c5fe46d5463080dce7f3b2cbd7b8ca
ismael29h <ismael29h@gmail.com>
2022-10-30 19:37:00 -0300

    Pulido: spawn rele

--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -36,10 +36,15 @@ func contabilizar_bases_enemigas() -> int:
 
 func crear_rele() -> void:
 	var nuevo_rele_masa:ReleMasa = rele_masa.instance()
-	nuevo_rele_masa.global_position = player.global_position + crear_posicion_aleatoria(1000.0, 800.0)
+	var pos_aleatoria:Vector2 = crear_posicion_aleatoria(400.0, 200.0)
+	var margen:Vector2 = Vector2(600.0, 600.0)
+	if pos_aleatoria.x < 0:
+		margen.x *= -1
+	if pos_aleatoria.y < 0:
+		margen.y *= -1
+	
+	nuevo_rele_masa.global_position = player.global_position + (margen + pos_aleatoria)
 	add_child(nuevo_rele_masa)
-	print("paso la prueba")
-
 
 
 func conectar_seniales() -> void:
