d44ccd577f7377d47e28db4536a779b91fb72127
ismael29h <ismael29h@gmail.com>
2022-10-30 19:29:26 -0300

    Pulido: arreglo de bug

--- Juego/Autoloads/DatosJuego.gd
+++ Juego/Autoloads/DatosJuego.gd
@@ -3,6 +3,15 @@ extends Node
 
 var player_actual:Player = null setget set_player_actual, get_player_actual
 
+func _ready() -> void:
+	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
+
+
+func _on_nave_destruida(nave:NaveBase, _posicion, _explosiones) -> void:
+	if nave is Player:
+		player_actual = null
+
+
 # setters - getters
 func set_player_actual(player:Player) -> void:
 	player_actual = player
