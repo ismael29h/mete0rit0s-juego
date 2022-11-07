eb79fe2ca1c4fed7048c4d4c3d8b6b6ab6a0ce9b
ismael29h <ismael29h@gmail.com>
2022-11-06 22:55:01 -0300

    Niveles hasta el 10 agregado - botón salir agregado al menú principal

--- Juego/UI/MenuPrincipal/MenuPrincipal.gd
+++ Juego/UI/MenuPrincipal/MenuPrincipal.gd
@@ -4,9 +4,14 @@ extends Node
 export(String, FILE, "*.tscn") var nivel_inicial = ""
 
 func _ready() -> void:
+	OS.set_window_fullscreen(true)
 	MusicaJuego.play_musica(MusicaJuego.get_lista_musicas().menu_principal)
 
 
 func _on_BotonJugar_pressed():
 	MusicaJuego.play_button()
 	get_tree().change_scene(nivel_inicial)
+
+
+func _on_BotonSalir_pressed():
+	get_tree().quit()
